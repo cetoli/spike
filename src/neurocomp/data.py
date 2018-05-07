98a8d443297c1958466ecd23ec6fdabe7162e58f
carlo <carlo@nce.ufrj.br>
2018-05-06 23:12:36 -0300

    neurocomp spike do banco de participantes

---
+++ src/neurocomp/data.py
@@ -0,0 +1,205 @@
+data = """
+30©Nome: Carla Verônica Machado Marques
+CPF: 011.121.767-99
+Endereco: Rua Silveira Martins 80 apto 202 Flamengo - Rio de Janeiro
+CEP: 24.210-110
+Telefone: 99400-5277
+Doutorado em Engenharia de Sistemas e Computação
+Email: carlaveronica@nce.ufrj.br
+Nascimento: 21/10/1968
+31©Nome: Carlo Emmanoel Tolla Oliveira
+CPF: 639.263.007-44
+Endereco: Rua Doutor Celestino 152 apto 1104 Centro Niterói
+CEP: 24.020-091
+Telefone: 996023084
+Doutorado em Ciência da Computação
+Email: carlo@nce.ufrj.br
+Nascimento: 11/06/1958
+32©Nome: Claudia Lage Rebello da Motta
+CPF: 756.239.197-15
+Endereco: Rua Silveira Martins 80 apto 202 Flamengo - Rio de Janeiro
+CEP: 24.210-110
+Telefone: 99400-5277
+Doutorado em Ciência da Computação
+Email: claudiam@nce.ufrj.br
+Nascimento: 21/10/1968
+1©Nome:  João Lopes Gasparino Junior
+CPF: 150.467.227-55
+Endereco: Rua Coronel Porto Carrero, n° 35, Bairro: Paciência
+CEP: 23580-010
+Nascimento: 07/06/1993
+2©Nome:  Rayane Beltrão Alves Cerqueira
+DRE: 117276875
+CPF: 170.111.997-80
+Endereco: R. Indaiaçu Leite, 128 Inhaúma BL 8 apto 112
+nascimento: 07/05/1999
+Email: omgrayane@gmail.com
+3©Nome:  Saridielly Sarah Floresta Soares
+CPF: 121.559.357-06
+Endereco: Rua Jorge Martins, quadra LM, bloco 26, apto 104 Bairro: Olaria
+CEP: 21073-190
+Nascimento:12/12/1988
+4©Nome:  Rafael da Silva Pizzo Cardoso
+DRE: 117256184
+CPF: 176.680.887-50
+Endereco: Travessa Doutor Alexandre Plemont; n° 22 casa 2; Bairro Vasco da Gama
+CEP: 20921-070
+nascimento: 20/04/1999
+Email: Pizzocardoso@outlook.com
+5©Nome:  Danielle Veloso Rodrigues 
+DRE:117230340
+CPF: 186.038.267-30
+Endereco: Rua Antonio Costa Magalhaes- Jacarepagua 
+nascimento: 06/08/1999
+Email: rodrigues.danielle5@gmail.com
+6©Nome:  Sara Hellen de Souza Matos
+CPF: 161.619.577-01
+Endereco: Rua Itaocara, n 188 casa 02 Jardim Marileia - Rio das Ostras
+CEP: 28896-043
+Nascimento: 08/06/1999
+7©Nome:  Beatriz Cristina Evangelista da Costa
+DRE: 117212350
+CPF: 169.140.127-76
+Endereco: Avenida Santa Cruz 833, Bloco 5 Apartamento 504
+nascimento: 12/03/1999
+Email: biacris5@yahoo.com.br
+8©Nome:  Tayza Aparecida Sodré
+CPF: 126.638.096-59
+Endereco: Estrada do Dendê 995; Apto 203 - Moneró
+CEP: 21920-001
+Nascimento: 12/10/1995
+Email: tayza.sodre@hotmail.com
+9©Nome:  Juliane de Jesus Pereira
+DRE: 117221642
+CPF: 178.182.767.26
+Endereco: Rua Pacheco Leão, 2036 casa 06; Bairro: Jardim Botânico
+nascimento: 25/01/1997
+Email: jufies888@hotmail.com
+10©Nome:  Leniah Lima Teixeira 
+DRE:117196766
+CPF: 454.152.758-86
+Endereco: Rua Dalia, n° 22, Vila residencial- Cidade Universitária
+nascimento: 20/11/1996
+Email: Leniahlima@gmail.com
+11©Nome:  Matheus Henrique Fernandes
+CPF: 455.934.708-55
+DRE: 117208652
+Endereco: R. Palmas, 4 - cidade universitária
+nascimento: 03/06/1998
+Email: matheushfernandes01@gmail.com
+12©Nome:  Dafne Moreira de Figueiredo Santos
+CPF: 173.730.127-01
+Endereco.: Estrada dos Caramujos, 1361 - Belmonte - Queimados
+CEP: 26420-270
+nascimento: 19/11/1997
+Email: moreira.dafne@yahoo.com.br
+13©Nome:  Dayana Vieira Marques
+DRE: 117244446
+CPF: 147.516.297-94
+Endereco: Rua Constante Ramos 53, Ap: 1104. Bairro: Copacabana
+Nascimento: 23/08/1991
+Email: dayana_marquees@hotmail.com
+14©Nome:  Alexsandra Vianna Gonçalves 
+Endereco: Fagundes Varela 240 -Niterói / Inga 
+CPF: 159.182.477-00
+nascimento: 14/10/1999
+Email: alexsandravianna@yahoo.com.br
+15©Nome:  Sarah Maria dos Santos 
+CPF: 123.424.057-23
+Endereco: Avenida Pernambucana, 1860- Vila Rosali -SJM 
+CEP:25510-430
+nascimento: 24/04/1999
+16©Nome:  Leidiene Deolindo dos Santos
+DRE: 117051348
+CPF: 063.288.977-23
+Endereco: Rua Caçu, 151. Bairro: Taquara
+nascimento: 30/08/1998
+17©Nome:  Emanuelle Marques Pereira Simas
+Nascimento: 04/12/1997
+DRE: 116154313
+CPF: 176070997-27
+Endereco: Rua Dr. Garnier, 417, casa
+Bairro: Rocha
+Email: ellesimas@gmail.com
+18©Nome:  Nathálya de Souza Santos
+DRE: 117050279
+CPF: 428.585.388-40
+Endereco: Rua Maurício da Costa Faria, 685, ap 103. Recreio dos bandeirantes
+nascimento: 23/10/1998
+Email: nathalyass98@gmail.com
+19©Nome:  Mariane Sampaio de Moraes
+DRE: 117209666
+Endereco: Av. General Carlos Marciano de Medeiros, n°623- Parque Fluminense, Duque de Caxias 
+Email: mari.sampaio1997@gmail.com
+CPF: 162.002.527-26
+20©Nome: DANIEL SANTOS CHAVES
+nascimento 18/05/1992
+CPF: 133.540.157-12
+Endereco: Rua Ituverava, 866, Anil – CEP: 22.750-006 – Rio de Janeiro – RJ.
+21©Nome:  Cibele Ribeiro da Cunha Oliveira
+nascimento:
+CPF:10752346709
+Endereco:
+22©Nome: Dayara Santos Carvalho
+CPF: 11764050630
+Endereco: Rua Visconde de Itamarati, 97 Ap. 304 - Maracanã - RJ
+CEP : 20550-140
+Email: dayara.s.carvalho@gmail.com
+Nascimento: 29/07/1995
+23©Nome: Ana Paula Cavadas 
+CPF: 02319951725
+24©Nome: Bruna Fiuza do Espirito Santo Silva
+CPF 14369673763
+25©Nome: Isabel Hortencia Garnica Perez Barros
+Endereco: Praça Comandante Xavier de Brito, 18 Cobertura 1 - Tijuca
+CEP: 20511-400
+Telefone: 24 988284342
+CPF: 01203528779
+26©Nome: Érica de Jesus Soares Scheffel
+CPF: 053.834.617-52
+Rua Montevideu, 1101 ap. 302 Bl 2 Penha Rio de Janeiro
+CEP: 21020-290
+27©Nome: Victor Antonio Azevedo Costa santos
+CPF: 025.099.785-19
+Endereco: Rua dos Prazeres, 144 apt. 308 Bl 2B Área A - Taquara
+Email: victorvaacs@gmail.com
+Nascimento: 01/08/1985
+28©Nome: Camila Assis da Silva
+CPF: 162.490.497-11
+Nascimento: 07/09/1996
+Graduação: Ciências Matemáticas  e da Terra
+Endereco: Rua Santa Isabel, 118 - Bairro Palhada - Nova Iguaçu
+CEP: 26.290-102
+Email: camila.assissantana@gmail.com
+29©Nome: Verônica Sakane Matias
+CPF: 142.113.857-39
+Endereco: Rua São Sebastião, 106 ap. 203 bl 02 - Ingá - Niterói
+CEP: 24.210-110
+Telefone: 99400-5277
+Bacharelado em Ciência da Computação
+Email: veronica.matias@nce.ufrj.br
+Nascimento: 10/03/1993
+"""
+
+
+class Parse:
+    def __init__(self):
+        self.campos = "Nome CPF DRE Endereco Email Nascimento Telefone CEP Bairro municipio titulo curso".split()
+        self.data = {k: "" for k in self.campos}
+        self.banco = []
+        
+    def parse(self):
+        def recorder(rec):
+            record=rec.splitlines()
+            dater = {key: value.split("{}: ".format(key))[1] for value in record for key in self.campos if key in value}
+            dat = dict(self.data)
+            dat.update(dater)
+            return dat
+        records = data.split("©")
+        self.banco = [recorder(rc) for rc in records]
+        
+        
+if __name__ == "__main__":
+    parser = Parse()
+    parser.parse()
+    print(parser.banco)
